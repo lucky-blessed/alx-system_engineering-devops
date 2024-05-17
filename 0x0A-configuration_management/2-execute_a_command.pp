ddf0a91283f40628b9f1745c23eb06fa963102ea
lucky-blessed <blessednkwor7@gmail.com>
2024-05-17 19:12:53 +0100

    0-create_a_file.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
-# kill process killmenow
-
-exec { 'pkill':
-  command  => 'pkill killmenow',
+# kill process killmenow && work with the killmenow file
+exec { 'killmenow':
+  command  => '/usr/bin/pkill killmenow',
   provider => 'shell',
+  return   =>  [o, 1],
 }
